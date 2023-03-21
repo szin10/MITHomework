206d847f0bae41f96fe5a214844959ae8e3e7a95
Sergey Zinoviev <szin10@yandex.ru>
2023-03-21 13:56:46 +0300

    Now fibonacci numbers are calculated iteratively. Each of them is shown on the screen

--- main.cpp
+++ main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-int fib(int n) {
-	if (n == 1)
-		return 0;
-	else if (n == 2)
-		return 1;
-	else
-		return fib(n - 1) + fib(n - 2);
-}
+#include <vector>
 
 int main() {
 	std::cout << "Hello, world!" << std::endl;
 	int n;
 	std::cin >> n;
-	std::cout << fib(n) << std::endl;
+	std::vector<int>a(n + 1);
+	a[0] = 0;
+	a[1] = 1;
+	for (int i = 2; i < n; ++i)
+		a[i] = a[i - 1] + a[i - 2];
+	for (int i = 0; i < n; ++i)
+		std::cout << a[i] << std::endl;
 	return 0;
 }
