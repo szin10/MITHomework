dedf8c2f3c4e0a8e9c644e755fdee8aeea0ad7a6
Sergey Zinoviev <szin10@yandex.ru>
2023-03-21 13:50:16 +0300

    Added function fib(n)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+int fib(int n) {
+	if (n == 1 || n == 2)
+		return 1;
+	else
+		return fib(n - 1) + fib(n - 2);
+}
 
 int main() {
 	std::cout << "Hello, world!" << std::endl;
+	int n;
+	std::cin >> n;
+	std::cout << fib(n) << std::endl;
 	return 0;
 }
